329be9f90af0f2dc45cd83bf3c0ff24955e12232
Joseph Daigle <jdaigle@users.noreply.github.com>
2018-10-11 07:42:40 -0400

    Update db_buffer_usage_v3.sql

--- memory/db_buffer_usage_v3.sql
+++ memory/db_buffer_usage_v3.sql
@@ -1,15 +1,17 @@
 WITH buffer_aggregate AS (
     SELECT
         DB_NAME(database_id) AS database_name,
-        (COUNT_BIG(*) * 8.0) / 1024 AS cached_mb
+        COUNT_BIG(*) AS count_pages,
+        (COUNT_BIG(*) * 8.0) / 1024 AS count_mb
     FROM sys.dm_os_buffer_descriptors WITH (NOLOCK)
     WHERE database_id <> 32767 -- ResourceDB
     GROUP BY database_id
 )
 SELECT
     database_name,
-    cached_mb,
-    cached_mb * 100 / SUM(cached_mb) OVER() AS cached_mb_percent
+    count_pages,
+    count_mb,
+    count_mb * 100 / SUM(count_mb) OVER() AS cached_mb_percent
 FROM buffer_aggregate
-ORDER BY cached_mb DESC
+ORDER BY count_pages DESC
 OPTION (RECOMPILE);
