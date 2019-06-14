a9bee62b531d968a979761a21f2ff2475638c465
Joseph Daigle <jdaigle@users.noreply.github.com>
2019-06-14 08:15:37 -0400

    Update db_index_usage_stats.sql

--- index/db_index_usage_stats.sql
+++ index/db_index_usage_stats.sql
@@ -3,21 +3,25 @@ SELECT
     o.[type_desc] AS object_type,
     COALESCE(i.name, '') AS index_name,
     i.type_desc AS index_type,
+    pstat.row_count,
+    pstat.used_page_count,
+    ((pstat.used_page_count * 8) / 1024.0) as used_mb_count,
     stat.user_seeks,
     stat.last_user_seek,
     stat.user_scans,
     stat.last_user_scan,
     stat.user_lookups,
     stat.last_user_lookup,
-    stat.last_user_update,
+    stat.user_lookups,
     stat.last_user_update
-FROM sys.dm_db_index_usage_stats stat
-    INNER JOIN sys.objects o WITH (NOLOCK)
-        ON o.object_id = stat.object_id 
+FROM sys.objects o WITH (NOLOCK)
     INNER JOIN sys.indexes i WITH (NOLOCK)
-        ON i.object_id = stat.object_id AND i.index_id = stat.index_id
-WHERE
-        stat.database_id = DB_ID('CHANGE ME')
+        ON i.object_id = o.object_id
+    INNER JOIN sys.dm_db_partition_stats pstat
+        ON pstat.object_id = i.object_id and pstat.index_id = i.index_id
+    LEFT JOIN sys.dm_db_index_usage_stats stat
+        ON stat.object_id = i.object_id AND stat.index_id = i.index_id
+WHERE o.object_id = OBJECT_ID('CHANGE ME OR REMOVE')
 ORDER BY
     o.name,
     i.type_desc,
